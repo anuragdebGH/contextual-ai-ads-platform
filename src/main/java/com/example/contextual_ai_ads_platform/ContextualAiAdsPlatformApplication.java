5932e47916125813e836f8d5083c617ce75776a3
Anurag Deb <anuragdeb@Anups-Air.modem>
2024-12-14 20:33:27 +1100

    Initial commit: Project scaffolded using Spring Initializr

---
+++ src/main/java/com/example/contextual_ai_ads_platform/ContextualAiAdsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.contextual_ai_ads_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextualAiAdsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextualAiAdsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/contextual_ai_ads_platform/ContextualAiAdsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contextual_ai_ads_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextualAiAdsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
